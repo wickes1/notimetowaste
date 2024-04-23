0b05869b1826fac08f0adc7f40d0dcb268f38fa8
Wickes1 <27881570+wickes1@users.noreply.github.com>
2024-04-22 20:33:41 -0400

    ai-saas: init

---
+++ fullstack/fullstack-ai-saas/README.md
@@ -0,0 +1,5 @@
+# Fullstack AI SAAS
+
+## Tutorial
+
+[GitHub - adrianhajdin/ai_saas_app: Build a REAL Software-as-a-Service app with AI features and payments & credits system that you might even turn into a side income or business idea using Next.js 14, Clerk, MongoDB, Cloudinary AI, and Stripe.](https://github.com/adrianhajdin/ai_saas_app)
---
+++ fullstack/fullstack-ai-saas/app/layout.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { IBM_Plex_Sans } from 'next/font/google';
+import './globals.css';
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex',
+});
+
+export const metadata: Metadata = {
+  title: 'Imaginify',
+  description: 'AI-powered image recognition for the web',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: '#624cf5' },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn('font-ibm-plex antialiased', IBMPlex.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
